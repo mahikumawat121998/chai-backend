1af97bb14b63f3b5b2e204ad3880036b20dad62e
MAHESH KUMAWAT <mahikumawat121998@gmail.com>
2024-03-05 14:58:24 +0530

    Commented code

--- src/app.js
+++ src/app.js
@@ -37,6 +37,10 @@ app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
 
-// http://localhost:8000/api/v1/users/register
+ 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
 
 export { app }
